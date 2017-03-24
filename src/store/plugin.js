6aaf1e2d8c92ee94b4f7cdcaf76b0f9a84c84119
flyerjay <876173839@qq.com>
2017-03-24 11:11:07 +0800

    更新代码

--- src/store/plugin.js
+++
@@ -1,9 +0,0 @@
-export default function loadListen(){
-    return store => {
-        store.subscribe(mutation=>{
-            if(mutation.type == 'LOAD_DOWN'){
-                
-            }
-        })
-    }
-}
