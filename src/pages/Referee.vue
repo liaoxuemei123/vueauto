5255d90ff75f5ee59626dd8c0b74c7d962c557cd
flyerjay <876173839@qq.com>
2017-06-16 09:48:05 +0800

    新增推荐人页面

---
+++ src/pages/Referee.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="page-container">
+        <div class="page home-page" flex="dir:top box:first">
+            <nav-bar
+                title="推荐人"
+            />
+            <div class="page-content" flex="dir:top">
+                <div class="input-control" flex="dir:left box:first">
+                    <div class="label">推荐类型</div>
+                    <div class="select-item" v-for="item in referees" @click="refereeType = item.value">
+                        <i class="iconfont" :class="refereeType == item.value ?'icon-select' : 'icon-circle'"></i>{{item.name}}
+                    </div>
+                </div>
+                <div class="inp-control" v-if="refereeType == 1">
+                    <inp-com
+                        title="推荐人"
+                        placeholder="填写推荐人手机号"
+                        :value="refereePhone"
+                        :onBlur="updatePhone.bind(this)"
+                    />
+                </div>
+                <div class="inp-control" v-if="refereeType == 0">
+                    <inp-com
+                        title="推荐服务门店"
+                        placeholder="请选择服务门店"
+                        :rightArrow="true"
+                        :readonly="true"
+                        :onClick="goStore.bind(this)"
+                        :value="storeName"
+                    />
+                </div>
+                <div class="button" @click="submitReferee">
+                    确定
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import NavBar from '../components/NavBar';
+    import Tool from '../utils/Tool';
+    import InpCom from '../components/InpCom';
+    import { mapState } from 'vuex';
+    export default{
+        data () {
+            return {
+                refereeType:0,
+                referees:[
+                    {
+                        name:"服务门店",
+                        value:0,
+                    },
+                    {
+                        name:"个人",
+                        value:1
+                    }
+                ],
+                storeName:"",
+                refereePhone:""
+            }
+        },
+        components:{
+            NavBar,
+            InpCom
+        },
+        computed:{
+            ...mapState(['packageInfo','refereeStore'])
+        },
+        mounted:function(){
+        },
+        activated:function(){
+            if(this.refereeStore.storeName){
+                this.storeName = this.refereeStore.storeName;
+            }else{
+                if(this.packageInfo.storeInfo.storeName){
+                    this.storeName = this.packageInfo.storeInfo.storeName;
+                }else{
+                    this.storeName = '';
+                }
+            }
+        },
+        methods:{
+            goStore:function(){
+                const wbpId = this.packageInfo.setInfo.wbpId;
+                this.$router.push({name:'store',params:{wbpId}});
+            },
+            submitReferee:function(){
+                if(this.refereeType == 0){
+                    this.$store.commit("SET_PACKAGE_USERINFO",{refereeType:this.refereeType,referee:this.storeName})
+                }else{
+                    this.$store.commit("SET_PACKAGE_USERINFO",{refereeType:this.refereeType,referee:this.refereePhone})
+                }
+                this.$router.back();
+            },
+            updatePhone:function(e){
+                this.refereePhone = $(e.target).val();
+            },
+        },
+        filters: {
+        },
+        beforeRouteEnter: (to, from, next) => {
+            Tool.routerEnter(to,from,next);
+        }
+    }
+</script>
+<style lang="less" scoped>
+    .page-container{
+        height:100%;
+        position:absolute;
+        width:100%;  
+    }
+    .page{
+        height:100%;
+        position:absolute;
+        width:100%;
+        .page-content{
+            background-color: #efefef;
+            height:100%;
+            overflow: auto;
+            .input-control{
+                height:1.8rem;
+                line-height:1.8rem;
+                background-color:#fff;
+                padding:0 3%;
+                margin-bottom:1px;
+                .label{
+                    width:40%;
+                    font-size:0.68rem;
+                }
+                .select-item{
+                    width:3rem;
+                    .iconfont{
+                        color:#ccc;
+                        margin-right:0.1rem;
+                    }
+                    .icon-select{
+                        color:#fc4c1d;
+                    }
+                }
+            }
+            .button{
+                position:absolute;
+                bottom:0;
+                left:0;
+                right:0;
+                height:2.1rem;
+                line-height:2.1rem;
+                font-size:0.64rem;
+                text-align:center;
+                background-color:#389cf2;
+                color:#fff;
+            }
+        }
+    }
+</style>
